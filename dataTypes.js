c481061b796609a2352742b86f466152c983cee3
Manishkumar551 <144582987+Manishkumar551@users.noreply.github.com>
2024-04-18 19:15:27 +0000

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat  all Js code as newer version
+
+//alert(3+3)  we are using nodejs,not browser
+
+console.log(3+3);
+//code readability should be high
+
+//Number
+//BigInt
+//String=> '' ""
+//boolean=>true,false
+//null=>standalone value
+//undefined->value is not assigned
+//symbol=>unique
+
+
+//object
+//array
+
+
+console.log(typeof "mani")//String
+console.log(typeof 23);//number
+
+console.log(typeof null)
+
+console.log(typeof undefined)//undefined
